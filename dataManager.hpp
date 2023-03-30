fc7891cc316b2327585f729edf2d62cb89e66c3e
TogliattiDrip <126583751+TogliattiDrip@users.noreply.github.com>
2023-03-30 19:51:38 +0200

    Add files via upload

---
+++ dataManager.hpp
@@ -0,0 +1,32 @@
+/*
+ * dataManager.hpp
+ *
+ *  Created on: 30 mar 2023
+ *      Author: lucar
+ */
+#include <iostream>
+#include <ncurses/ncurses.h>
+#include <cstring>
+#include <string>
+#include <ctime>
+#include "box.hpp"
+#include "player.hpp"
+#include <fstream>
+using namespace std;
+
+int save_data(player* P, int Seed, string filename);
+	/*
+	 * MEMO: savegame structure as follows (WIP)
+	 * Line1: yloc (code 0)
+	 * Line2: xLoc (code 1)
+	 * Line3: Life (code 4)
+	 * Line4: money (code 11)
+	 * Line5: map seed
+	 *
+	 *
+	 *
+	 */
+
+
+//void read_data(){}
+

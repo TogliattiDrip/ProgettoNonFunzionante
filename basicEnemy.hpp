418c2b3ba712c8c45edf7bf7bd6cfb0a4b0e8c65
TogliattiDrip <126583751+TogliattiDrip@users.noreply.github.com>
2023-05-19 17:02:39 +0200

    Update basicEnemy.hpp

--- basicEnemy.hpp
+++ basicEnemy.hpp
@@ -9,16 +9,65 @@ protected:
 	char enemy;
 	WINDOW * curwin;
 public:
-	int life;
-	basicenemy(WINDOW * win, int y, int x, int l, char e);
+	int life, money;
+	basicenemy(WINDOW * win, int y, int x, int l, char e, int m){
+		curwin=win;
+		yLoc=y;
+		xLoc=x;
+
+		//Vita del nemico
+		life=l;
+		enemy=e;
+		money=m;
+
+		//Indica se si sta sparando un proiettile
+		s=false;
+
+		//Coordinate x e y del proiettile
+		projx=0;
+		projy=0;
+
+		getmaxyx(curwin, yMax, xMax);
+		dirlock=0;
+
+		//setto rand
+		srand(time(0));
+	}
 	void display();
 	int playerfinder();
-	void mvup();
-	void mvdown();
 	void mvleft();
 	void mvright();
+	void mvdown();
 	void* behaviour(void*);
 	void shoot(int dir);
     bool isterrain(char t);
     void takedamage();
+    void gravity();
 };
+
+class jumpingenemy: public basicenemy{
+protected:
+	bool j=false;
+	int difficulty;
+public:
+	jumpingenemy(WINDOW * win, int y, int x, int l, char e, int m, int d) : basicenemy(win, y, x, l, e, m){
+		j=false;
+		difficulty=d;
+	}
+	void mvup();
+	void mvleft();
+	void mvright();
+	int playerfinder();
+	int bulletfinder();
+    void jump(int dir);
+	void* behaviour(void*);
+    void takedamage();
+
+};
+
+//Prende in input tre basicenemy e ne rilascia uno casualmente
+basicenemy* basic_enemy_randomizer (basicenemy* e1, basicenemy* e2, basicenemy* e3);
+
+//Una possibilità su quattro esca jumping enemy, nel caso esca, rilascia in output e,
+//in caso cotrario rilascia NULL
+jumpingenemy* jumping_enemy_randomizer (jumpingenemy* e);
